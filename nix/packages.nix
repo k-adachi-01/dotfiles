a86bee7208813d885b420e88cd81cc391dea2b26
k-adachi-01 <sewaniwa@gmail.com>
2026-06-16 01:31:54 +0900

    Add browser-use global commands

--- nix/packages.nix
+++ nix/packages.nix
@@ -1,5 +1,20 @@
 {pkgs}:
 with pkgs; let
+  browserUseCommand = name:
+    writeShellApplication {
+      inherit name;
+      runtimeInputs = [uv];
+      text = ''
+        export BROWSER_USE_CONFIG_DIR="''${BROWSER_USE_CONFIG_DIR:-$HOME/.browser-use-config}"
+        export BROWSER_USE_SOCKET_MODE="''${BROWSER_USE_SOCKET_MODE:-tcp}"
+        export PLAYWRIGHT_BROWSERS_PATH="''${PLAYWRIGHT_BROWSERS_PATH:-${playwright-driver.browsers}}"
+        export PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD="''${PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD:-1}"
+
+        exec uvx --from 'browser-use[cli]==0.12.1' browser-use "$@"
+      '';
+    };
+  browserUse = browserUseCommand "browser-use";
+  browserUseLocal = browserUseCommand "browser-use-local";
   kiroCliFixed = kiro-cli.overrideAttrs (oldAttrs: {
     postInstall =
       (oldAttrs.postInstall or "")
@@ -22,6 +37,8 @@ in [
   azure-cli
   alejandra
   bat
+  browserUse
+  browserUseLocal
   claude-code
   cmake
   codex
