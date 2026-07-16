a08da5d760a39b330e8adfa9976d648a89b924c4
k-adachi-01 <sewaniwa@gmail.com>
2026-07-17 06:43:39 +0900

    fix(nix): register Slack URL handler

--- nix/home.nix
+++ nix/home.nix
@@ -80,6 +80,47 @@ in {
 
   programs.home-manager.enable = true;
 
+  xdg = lib.mkIf (!isDarwin) {
+    enable = true;
+    mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "text/html" = ["google-chrome.desktop"];
+        "x-scheme-handler/http" = ["google-chrome.desktop"];
+        "x-scheme-handler/https" = ["google-chrome.desktop"];
+        "x-scheme-handler/about" = ["google-chrome.desktop"];
+        "x-scheme-handler/unknown" = ["google-chrome.desktop"];
+        "x-scheme-handler/slack" = ["slack.desktop"];
+      };
+    };
+    configFile."mimeapps.list".force = true;
+    dataFile."applications/slack.desktop" = {
+      force = true;
+      text = ''
+        [Desktop Entry]
+        Name=Slack
+        GenericName=Slack Client
+        Comment=Slack Desktop
+        Exec=${pkgs.slack}/bin/slack -s %U
+        Icon=slack
+        Type=Application
+        Terminal=false
+        StartupNotify=true
+        StartupWMClass=Slack
+        Categories=Network;InstantMessaging;
+        MimeType=x-scheme-handler/slack;
+      '';
+    };
+    dataFile."applications/mimeinfo.cache" = {
+      force = true;
+      text = ''
+        [MIME Cache]
+        x-scheme-handler/slack=slack.desktop;
+      '';
+    };
+    dataFile."applications/mimeapps.list".force = true;
+  };
+
   programs.git = {
     enable = true;
     settings = {
